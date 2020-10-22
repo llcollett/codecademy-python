8ad5b27b9726e9bf02a4bd9bce87dbc793b1c213
Laura Collett <la.lo.collett@gmail.com>
2020-10-22 11:58:40 +0100

    starting incorporating a game

--- jeopardy_starting/jeopardy.py
+++ jeopardy_starting/jeopardy.py
@@ -1,5 +1,7 @@
 
 import pandas as pd
+import datetime as dt
+import random
 pd.set_option('display.max_colwidth', None)
 
 # read in csv
@@ -48,3 +50,40 @@ def unique_answers(data, words):
 # test
 # print(unique_answers(jeopardy, ['women']))
 
+# use of word computer over time
+"""
+filtered_computer = word_filter(jeopardy, ['computer'])
+computer_90s = filtered_computer[
+	(filtered_computer['date'] > dt.datetime(1990, 1, 1)) 
+		& (filtered_computer['date'] < dt.datetime(2000, 1, 1))
+	]
+computer_90s_count = computer_90s.question.nunique()
+computer_00s = filtered_computer[
+	(filtered_computer['date'] > dt.datetime(2000, 1, 1)) 
+		& (filtered_computer['date'] < dt.datetime(2010, 1, 1))
+	]
+computer_00s_count = computer_00s.question.nunique()
+"""
+
+# examine
+#print("Computer was mentioned " + str(computer_90s_count) + " times in the 90s")
+#print("Computer was mentioned " + str(computer_00s_count) + " times in the 00s")
+
+# build a game
+def jeopardy_game(df = jeopardy):
+	correct = 0
+	incorrect = 0
+	random_index = random.randint(0, len(df))
+	question = df.question.iloc[random_index]
+	answer = df.answer.iloc[random_index]
+	print(question)
+	user_input = input('Give it your best shot: ')
+	if (user_input.lower() == answer.lower()):
+		print('Well done!')
+		correct += 1
+	else: 
+		print('Bad luck')
+		incorrect += 1
+	return 'You scored: ' + str(score)
+
+jeopardy_game()
